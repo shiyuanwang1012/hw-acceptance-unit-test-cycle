fe39e80944c30a5e315320d3f701149698bec60a
Shiyuan Wang <shiyuanwang1012@berkeley.edu>
2020-11-08 08:44:45 +0000

    for now

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -3,9 +3,9 @@ class Movie < ActiveRecord::Base
     Movie.where(:director => director)
   end
   
-  def self.similar_movies movie_title
-    director = Movie.find_by(title: movie_title).director
-    return nil if director.blank? or director.nil?
-    Movie.where(director: director).pluck(:title)
-  end
+  #def self.similar_movies movie_title
+    #director = Movie.find_by(title: movie_title).director
+    #return nil if director.blank? or director.nil?
+    #Movie.where(director: director).pluck(:title)
+  #end
 end
