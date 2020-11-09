857831b356bfd69b1279094ef7d839d651ba06c5
Shiyuan Wang <shiyuanwang1012@berkeley.edu>
2020-11-09 06:28:34 +0000

    3

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -2,10 +2,4 @@ class Movie < ActiveRecord::Base
   def self.same_directors director
     Movie.where(:director => director)
   end
-  
-  #def self.similar_movies movie_title
-    #director = Movie.find_by(title: movie_title).director
-    #return nil if director.blank? or director.nil?
-    #Movie.where(director: director).pluck(:title)
-  #end
 end
--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -24,8 +24,8 @@ module NavigationHelpers
     when /^the details page for "(.*)"$/i
       movie_path(Movie.find_by_title($1))
       
-    when /^the Similar Movies page for "(.*)"$/i
-      c(Movie.find_by_title($1).id)
+    #when /^the Similar Movies page for "(.*)"$/i
+      #c(Movie.find_by_title($1).id)
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
